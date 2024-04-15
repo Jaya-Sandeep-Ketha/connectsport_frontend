81d049b0dff7b6f30d23f95a51e930a8b30b3a2e
Jaya-Sandeep-Ketha <kethajayasandeep1254@gmail.com>
2024-04-15 12:24:55 -0400

    Messaging Fix

--- src/pages/Messaging/chatArea.js
+++ src/pages/Messaging/chatArea.js
@@ -5,21 +5,32 @@ import { useAuth } from '../../services/useAuth';
 const ChatArea = ({ activeChat, viewMode }) => {
     const [messages, setMessages] = useState([]);
     const { currentUser } = useAuth();
+    const fetchInterval = 5000; // Fetch every 5000 milliseconds (5 seconds)
 
     useEffect(() => {
-        if (currentUser && activeChat) {
-            const url = viewMode === 'groups'
-                ? `${process.env.REACT_APP_API_URL}/groups/${activeChat}/messages`
-                : `${process.env.REACT_APP_API_URL}/messages?senderId=${currentUser}&receiverId=${activeChat}`;
-            
-            fetch(url)
-                .then(response => response.json())
-                .then((data) => {
-                    setMessages(data);
-                })
-                .catch(console.error);
-        }
-    }, [currentUser, activeChat, viewMode]);
+        const fetchMessages = () => {
+            if (currentUser && activeChat) {
+                const url = viewMode === 'groups'
+                    ? `${process.env.REACT_APP_API_URL}/groups/${activeChat}/messages`
+                    : `${process.env.REACT_APP_API_URL}/messages?senderId=${currentUser}&receiverId=${activeChat}`;
+
+                fetch(url)
+                    .then(response => response.json())
+                    .then((data) => {
+                        setMessages(data);
+                    })
+                    .catch(console.error);
+            }
+        };
+
+        // Initialize message fetching and set up the interval
+        fetchMessages();
+        const intervalId = setInterval(fetchMessages, fetchInterval);
+
+        // Clean up the interval on component unmount
+        return () => clearInterval(intervalId);
+    }, [currentUser, activeChat, viewMode]); // Dependencies for effect
+
 
     const chatHeader = viewMode === 'groups' 
         ? `Group: ${activeChat}` 
